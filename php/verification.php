a0783c4b0f68f603a529ed762745d6cec2ba478f
Benedicte141 <benedicte.gady@sts-sio-caen.info>
2023-12-21 21:40:25 +0100

    MAJ 21/12/2023

--- php/verification.php
+++ php/verification.php
@@ -10,19 +10,9 @@ $message = ($_POST['message']);
 $headers = 'From: '.$nom.'<'.$mail.'>'."\r\n";
 $headers .= 'Reply-to: <'.$mail.'>'."\r\n";
 
-$to = 'benedicte.gady@sts-sio-caen.fr';
+$to = 'ebene.olia@hotmail.fr';
 
 ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Document sans titre</title>
-</head>
-
-<body>
-
 <?php
 
 if($_POST['captcha']==$_SESSION['captcha']){
@@ -31,15 +21,5 @@ echo 'Votre message a bien été envoyé';
 	
 }else{
 echo 'Le captcha entré est invalide. <a href="../index.php#contact">Recommencez</a>';	
-	
 }
-
-
-
 ?>
-
-
-</body>
-</html>
-
-
